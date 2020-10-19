cb9aa01e4e1244ab304fb37fc02f81d1c947fec2
Abed01-lab <abedharirii@gmail.com>
2020-10-19 21:01:16 +0200

    person endpoints

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -21,7 +21,11 @@ public class ApplicationConfig extends Application {
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
-        resources.add(rest.RenameMeResource.class);
+        resources.add(rest.AddressResource.class);
+        resources.add(rest.CityResource.class);
+        resources.add(rest.HobbyResource.class);
+        resources.add(rest.PersonResource.class);
+        resources.add(rest.PhoneResource.class);
     }
     
 }
