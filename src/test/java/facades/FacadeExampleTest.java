a727da8a198ed4cf7345308e19b028b6ecfa04b0
rh <cph-178rh@cphbusiness.dk>
2020-10-20 18:03:55 +0200

    Test af person

--- src/test/java/facades/FacadeExampleTest.java
+++ src/test/java/facades/FacadeExampleTest.java
@@ -1,7 +1,10 @@
 package facades;
 
+import DTO.PersonDTO;
+import entities.Address;
 import utils.EMF_Creator;
 import entities.Person;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import org.junit.jupiter.api.AfterAll;
@@ -17,14 +20,22 @@ public class FacadeExampleTest {
 
     private static EntityManagerFactory emf;
     private static PersonFacade facade;
+    private Person p1;
+    private Person p2;
 
     public FacadeExampleTest() {
     }
 
     @BeforeAll
     public static void setUpClass() {
-       emf = EMF_Creator.createEntityManagerFactoryForTest();
-       facade = PersonFacade.getFacade(emf);
+
+        emf = EMF_Creator.createEntityManagerFactoryForTest();
+        facade = PersonFacade.getFacade(emf);
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.createQuery("DELETE FROM Person p").executeUpdate();
+        em.createNativeQuery("ALTER TABLE `MEMBER` AUTO_INCREMENT = 1").executeUpdate();
+
     }
 
     @AfterAll
@@ -36,18 +47,50 @@ public class FacadeExampleTest {
     //TODO -- Make sure to change the code below to use YOUR OWN entity class
     @BeforeEach
     public void setUp() {
-     
+        p1 = new Person();
+      
+        p1.setEmail("john@email.dk");
+        p1.setFirstName("John");
+        p1.setLastName("JOhnson");
+
+        p2 = new Person();
+        p2.setEmail("Pete@mail.com");
+        p2.setFirstName("Pete");
+        p2.setLastName("Petersen");
+
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.createQuery("DELETE from Person").executeUpdate();
+            em.persist(p1);
+            em.persist(p2);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+
     }
 
     @AfterEach
     public void tearDown() {
-//        Remove any data after each test was run
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.createQuery("DELETE FROM Person p").executeUpdate();
+        em.createNativeQuery("ALTER TABLE `MEMBER` AUTO_INCREMENT = 1").executeUpdate();
+        em.getTransaction().commit();
     }
 
-    // TODO: Delete or change this method 
-    /*@Test
-    public void testAFacadeMethod() {
-        assertEquals(2, facade.getRenameMeCount(), "Expects two rows in the database");
-    }*/
-
+    
+    @Test
+    public void testGetAll() {
+        List<PersonDTO> result = facade.allPersons();
+        assertEquals(2, result.size());
+    }
+    
+    @Test
+    public void testGetById() {
+        int id = p1.getId();
+        PersonDTO p = facade.getUserById(id);
+        assertEquals("John", p.getFirstName() );
+    }
 }
